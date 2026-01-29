bd47a3f8059fb5780db814990337b852a011c1b9
Shakil <developerhubytchannel@gmail.com>
2026-01-30 03:00:24 +0600

    bug fix of hydration mismatch

--- app/components/public/common/ProfileMenu.vue
+++ app/components/public/common/ProfileMenu.vue
@@ -12,27 +12,31 @@ const { user, avatar, name, handleLogout } = useAuth();
 </script>
 
 <template>
-  <template v-if="user">
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Avatar class="ring-2 ring-primary">
-          <AvatarImage :src="avatar" :alt="name" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <NuxtLink to="/dashboard">
-          <DropdownMenuItem class="cursor-pointer"> Profile </DropdownMenuItem>
-        </NuxtLink>
-        <DropdownMenuItem variant="destructive" @click="handleLogout"
-          >Logout</DropdownMenuItem
-        >
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </template>
-  <template else>
-    <NuxtLink to="/login">
-      <Button>Login</Button>
-    </NuxtLink>
-  </template>
+  <ClientOnly>
+    <template v-if="user">
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Avatar class="ring-2 ring-primary">
+            <AvatarImage :src="avatar" :alt="name" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <NuxtLink to="/dashboard">
+            <DropdownMenuItem class="cursor-pointer">
+              Profile
+            </DropdownMenuItem>
+          </NuxtLink>
+          <DropdownMenuItem variant="destructive" @click="handleLogout"
+            >Logout</DropdownMenuItem
+          >
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </template>
+    <template else>
+      <NuxtLink to="/login">
+        <Button>Login</Button>
+      </NuxtLink>
+    </template>
+  </ClientOnly>
 </template>
