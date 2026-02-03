6ae1a4864e1e5c571651ba83ea33e81665d0801d
developerHub01 <shakil102043@gmail.com>
2026-02-03 21:07:57 +0600

    hero-animated-bg animation updated

--- app/components/public/home/HeroAnimatedBg.vue
+++ app/components/public/home/HeroAnimatedBg.vue
@@ -3,14 +3,14 @@
     <div
       class="absolute inset-0 bg-linear-to-b from-primary/10 via-primary/5 to-transparent"
     />
-    <div class="relative container mx-auto w-full h-full">
+    <div class="relative w-full h-full">
       <div
         ref="blob1Ref"
-        class="absolute top-1/4 left-1/4 w-96 h-96 bg-primary/20 rounded-full opacity-50 blur-3xl"
+        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[500px] h-[500px] bg-primary/20 rounded-full opacity-50 blur-[120px]"
       />
       <div
         ref="blob2Ref"
-        class="absolute bottom-1/4 right-1/4 w-96 h-96 bg-primary/10 rounded-full opacity-50 blur-3xl"
+        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[400px] h-[400px] bg-primary/10 rounded-full opacity-50 blur-[100px]"
       />
     </div>
   </div>
@@ -24,20 +24,24 @@ onMounted(() => {
   const { $gsap } = useNuxtApp();
 
   if ($gsap && blob1Ref.value && blob2Ref.value) {
-    $gsap.to(blob1Ref.value, {
-      scale: 1.2,
-      opacity: 0.8,
-      duration: 4,
-      repeat: -1,
-      yoyo: true,
-      ease: "sine.inOut",
-    });
+    const animateBlob = (el: HTMLElement) => {
+      $gsap.to(el, {
+        x: "random(-60, 60)vw",
+        y: "random(-60, 60)vh",
+        scale: "random(1.2, 2.5)",
+        duration: "random(10, 20)",
+        ease: "sine.inOut",
+        onComplete: () => animateBlob(el),
+      });
+    };
 
-    $gsap.to(blob2Ref.value, {
-      scale: 1.2,
+    animateBlob(blob1Ref.value);
+    animateBlob(blob2Ref.value);
+
+    // Initial scale/opacity pulses
+    $gsap.to([blob1Ref.value, blob2Ref.value], {
       opacity: 0.8,
-      duration: 4,
-      delay: 2,
+      duration: 5,
       repeat: -1,
       yoyo: true,
       ease: "sine.inOut",
