ca847e4ecd75ed54ddd7c2cf4d98dab6fee576d0
developerHub01 <shakil102043@gmail.com>
2026-06-06 01:44:17 +0600

    print mdx docs add

--- app/docs/testing/print/page.tsx
+++ app/docs/testing/print/page.tsx
@@ -1,5 +1,17 @@
-const Page = () => {
-  return <>print</>;
+import { Metadata } from "next";
+import Print from "@/app/docs/testing/print/content.mdx";
+
+export const metadata: Metadata = {
+  title: "print() method | APIBolt",
+  description:
+    "Learn how to use the print() method to inspect response state and environment variables during API test execution.",
+  openGraph: {
+    title: "print() method | APIBolt",
+    description:
+      "Learn how to use the print() method to inspect response state and environment variables during API test execution.",
+  },
 };
+
+const Page = () => <Print />;
 
 export default Page;
