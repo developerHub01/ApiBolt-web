2c2b77f7b0efe1a0d5569a93c529f492968b2ef9
developerHub01 <shakil102043@gmail.com>
2026-06-06 01:54:54 +0600

    Env mdx docs add

--- app/docs/testing/env/page.tsx
+++ app/docs/testing/env/page.tsx
@@ -1,5 +1,17 @@
-const Page = () => {
-  return <>env</>;
+import { Metadata } from "next";
+import Env from "@/app/docs/testing/env/content.mdx";
+
+export const metadata: Metadata = {
+  title: "Environment Access | APIBolt",
+  description:
+    "Learn how to access environment variables directly in API test scripts using ab.env for runtime validation and configuration.",
+  openGraph: {
+    title: "Environment Access | APIBolt",
+    description:
+      "Learn how to access environment variables directly in API test scripts using ab.env for runtime validation and configuration.",
+  },
 };
+
+const Page = () => <Env />;
 
 export default Page;
