d594ac025f86bdbb7c83be22f7c4c4d6c5ac67a1
developerHub01 <shakil102043@gmail.com>
2026-06-10 01:06:01 +0600

    header style update

--- components/app/common/header/Header.tsx
+++ components/app/common/header/Header.tsx
@@ -13,7 +13,8 @@ const Header = ({ className, ...props }: Props) => {
   return (
     <section
       className={cn(
-        "sticky top-0 w-full flex items-center justify-center py-4 z-50 transition-all duration-300 pointer-events-none bg-linear-to-b from-secondary/70 via-secondary/40 to-transparent",
+        "sticky top-0 w-full flex items-center justify-center py-4 z-50 transition-all duration-300 pointer-events-none",
+        "bg-linear-to-b from-secondary/65 via-secondary/35 to-transparent",
         className,
       )}
       {...props}
