492e0e01762d220f5ef24b2419acbe5e09e02c93
developerHub01 <shakil102043@gmail.com>
2026-06-29 19:52:34 +0600

    feat: integrate Google Generative AI and update ask query handling

--- server/v1/modules/ai/ai.controller.ts
+++ server/v1/modules/ai/ai.controller.ts
@@ -1,20 +1,27 @@
 import { AskQueryBodyInterface } from "@/types/ai.types";
 import { HTTPContext } from "@/types/server/env.types";
 import { sendResponse } from "@/utils/server/api";
+import { generateText } from "ai";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
 
-const API_KEY = process.env.ASK_AI_API_KEY;
+const API_KEY = process.env.ASK_AI_API_KEY!;
+
+const google = createGoogleGenerativeAI({
+  apiKey: API_KEY,
+});
 
 const handleAskQuery = async (c: HTTPContext) => {
-  const body = c.req.json<AskQueryBodyInterface>();
+  const { message } = await c.req.json<AskQueryBodyInterface>();
 
-  console.log({
-    API_KEY,
+  const { text } = await generateText({
+    model: google("gemini-3-flash-preview"),
+    prompt: message,
   });
 
   return sendResponse(c, {
     statusCode: 200,
     message: "AI message got successfully",
-    data: body,
+    data: text,
   });
 };
 
